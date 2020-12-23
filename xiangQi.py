ad81f80b69090af9b88ea0bef3e9510e130d86dd
joycemaferko <joycema@oregonstate.edu>
2020-12-23 19:04:14 +0100

    Added board and initialized all pieces on it

--- xiangQi.py
+++ xiangQi.py
@@ -10,7 +10,24 @@ class XiangqiGame:
 
         self._game_state = "UNFINISHED"
         self._player_turn = "red"
-        self._board = []
+        self._board = [
+            [Chariot("Red", "a1", "r R"), Horse("Red", "b1", "r H"), Elephant("Red", "c1", "r E"), Adviser("Red", "d1", "r A"),
+                 General("Red", "e1", "r G"), Adviser("Red", "f1", "r A"), Elephant("Red", "g1", "r E"), Horse("Red", "h1", "r H"),
+                 Chariot("Red", "i1", "r R")],
+            [None, None, None, None, None, None, None, None, None],
+            [None, Cannon("Red", "b3", "r C"), None, None, None, None, None, Cannon("Red", "h3", "r C"), None],
+            [Soldier("Red", "a4", "r S"), None, Soldier("Red", "c4", "r S"), None, Soldier("Red", "e4","r S"), None,
+                 Soldier("Red", "g4", "r S"), None, Soldier("Red", "i4", "r S")],
+            [None, None, None, None, None, None, None, None, None],
+            [None, None, None, None, None, None, None, None, None],
+            [Soldier("Black", "a7","b S"), None, Soldier("Black", "c7","b S"), None, Soldier("Black", "e7","b S"), None,
+                 Soldier("Black", "g7","b S"), None, Soldier("Black", "i7","b S")],
+                [None, Cannon("Black", "b8", "b C"), None, None, None, None, None, Cannon("Black", "h8", "b C"), None],
+                [None, None, None, None, None, None, None, None, None],
+                [Chariot("Black", "a10","b R"), Horse("Black", "b10","b H"), Elephant("Black", "c10","b E"), Adviser("Black", "d10","b A"),
+                 General("Black", "e10","b G"), Adviser("Black", "f10","b A"), Elephant("Black", "g10","b E"), Horse("Black", "h10","b H"),
+                 Chariot("Black", "i10","b R")]
+        ]
 
     def get_player_turn(self):
         """returns the color of player whose turn it is"""
@@ -19,6 +36,23 @@ class XiangqiGame:
     def set_player_turn(self, color):
         """sets the color of the player whose turn it is"""
         self._player_turn = color
+
+    def print_board(self):
+        """Prints game board."""
+
+        columns = "   a     b     c     d     e     f     g     h     i"
+        top = "┏━" + ''.join("━━━━┯━" for _ in range(8)) + '━━━━┓'
+        bot = "┗━" + ''.join("━━━━┷━" for _ in range(8)) + '━━━━┛'
+        sep = "┠─" + ''.join("────┼─" for _ in range(8)) + '────┨'
+
+        def r(row):
+            return "┃" + "│".join(f" {repr(GamePiece)} " if GamePiece else "     " for GamePiece in row) + "┃"
+
+        top_mid = f"\n{sep}\n".join(r(self._board[i]) + " " + str(i + 1) for i in range(5))
+        river = f"{bot}\n\033[0;34m░░░░░░░░░░░░░░░░░░░░░░░░░River░░░░░░░░░░░░░░░░░░░░░░░░░\033[0m\n{top}"
+        bot_mid = f"\n{sep}\n".join(r(self._board[i]) + " " + str(i + 1) for i in range(5, 10))
+
+        print(f"{columns}\n{top}\n{top_mid}\n{river}\n{bot_mid}\n{bot}\n{columns}")
 
     def is_in_check(self, color):
         """Returns True if given color is in check, otherwise returns False"""
@@ -74,25 +108,30 @@ class Chariot(GamePiece):
     pass
 
 class Horse(GamePiece):
-    """Creates Chariot sub-class"""
+    """Creates Horse sub-class"""
     pass
 
 class Elephant(GamePiece):
-    """Creates Chariot sub-class"""
+    """Creates Elephant sub-class"""
     pass
 
 class Adviser(GamePiece):
-    """Creates Chariot sub-class"""
+    """Creates Adviser sub-class"""
     pass
 
 class General(GamePiece):
-    """Creates Chariot sub-class"""
+    """Creates General sub-class"""
     pass
 
 class Cannon(GamePiece):
-    """Creates Chariot sub-class"""
+    """Creates Cannon sub-class"""
     pass
 
 class Soldier(GamePiece):
-    """Creates Chariot sub-class"""
+    """Creates Soldier sub-class"""
     pass
+
+game = XiangqiGame()
+
+game.print_board()
+
